41a27bcda159a87163259e5d62d931ff81744208
Dest1n1s <dest1nis2022@gmail.com>
2022-10-04 20:20:02 +0800

    fix: optim test & warn for specialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(incomplete_features)]
 #![feature(specialization)]
 
 pub mod nn;
--- tests/optim_test.rs
+++ tests/optim_test.rs
@@ -7,16 +7,16 @@ use raddar::optim::optimizer::Optimizer;
 #[test]
 fn gradient_descent_test() {
     let inputs = Tensor::of_slice2(&[[1.0], [3.0], [5.0], [4.0], [8.0], [10.0], [2.0], [6.0]]);
-    let labels = Tensor::of_slice2(&[[3.0], [7.1], [10.5], [9.0], [16.6], [20.8], [5.6], [13.0]]);
+    let labels = Tensor::of_slice2(&[[4.0], [10.0], [16.], [13.0], [25.], [31.], [7.], [19.0]]);
 
-    let model = vec![Box::new(Linear::new(1, 1, true))];
-    let optimizer = Optimizer::new(GradientDescent::new(0.001), &model);
-    for epoch in 1..=500 {
+    let model = Linear::new(1, 1, true);
+    let optimizer = Optimizer::new(GradientDescent::new(0.01), &model);
+    for epoch in 1..=5000 {
         let loss = model.forward(&inputs).mse_loss(&labels, Reduction::Mean);
         loss.backward();
         optimizer.step();
         println!("epoch: {}, loss: {}", epoch, f64::from(loss));
     }
     println!("final model:");
-    // println!("weight: {}, bias: {}", f64::from(&*model.weight.lock().unwrap()), f64::from(&*model.bias.unwrap().lock().unwrap()));
+    println!("weight: {}, bias: {}", f64::from(&*model.weight.lock().unwrap()), f64::from(&*model.bias.unwrap().lock().unwrap()));
 }
