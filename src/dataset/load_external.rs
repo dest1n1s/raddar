5963887d5ba94a3fc1324aee5be88aad9d73711b
Dest1n1s <dest1nis2022@gmail.com>
2022-10-22 00:06:49 +0800

    feat: json loader

---
+++ src/dataset/load_external.rs
@@ -0,0 +1,63 @@
+use std::sync::Arc;
+
+use super::{SimpleDataset, UnsupervisedDataset};
+pub trait LoadFromJson {
+    type ConfigType;
+
+    fn from_json(path: &str, config: Self::ConfigType) -> Self;
+}
+
+pub struct SimpleDatasetJsonConfig {
+    pub input_field: String,
+    pub label_field: String,
+}
+
+impl<InputType, LabelType> LoadFromJson for SimpleDataset<InputType, LabelType> where InputType: serde::de::DeserializeOwned, LabelType: serde::de::DeserializeOwned {
+    type ConfigType = SimpleDatasetJsonConfig;
+
+    fn from_json(path: &str, config: Self::ConfigType) -> Self {
+        let file = std::fs::File::open(path).expect(format!("Failed to open file {}", path).as_str());
+        let reader = std::io::BufReader::new(file);
+        let mut inputs = Vec::new();
+        let mut labels = Vec::new();
+        let json: serde_json::Value = serde_json::from_reader(reader).expect("Failed to parse json");
+        for item in json.as_array().expect("Input file is not a valid JSON array") {
+            let input = item.get(&config.input_field).expect("Input field not found in JSON object");
+            let label = item.get(&config.label_field).expect("Label field not found in JSON object");
+            inputs.push(Arc::new(serde_json::from_value(input.clone()).expect("Input field is not compatible with the specified type")));
+            labels.push(Arc::new(serde_json::from_value(label.clone()).expect("Label field is not compatible with the specified type")));
+        }
+        let size = inputs.len();
+        SimpleDataset {
+            inputs,
+            labels,
+            size,
+            batch_size: 1,
+        }
+    }
+}
+
+pub struct UnsupervisedDatasetJsonConfig {
+    pub input_field: String,
+}
+
+impl<InputType> LoadFromJson for UnsupervisedDataset<InputType> where InputType: serde::de::DeserializeOwned {
+    type ConfigType = UnsupervisedDatasetJsonConfig;
+
+    fn from_json(path: &str, config: Self::ConfigType) -> Self {
+        let file = std::fs::File::open(path).expect(format!("Failed to open file {}", path).as_str());
+        let reader = std::io::BufReader::new(file);
+        let mut inputs = Vec::new();
+        let json: serde_json::Value = serde_json::from_reader(reader).expect("Failed to parse json");
+        for item in json.as_array().expect("Input file is not a valid JSON array") {
+            let input = item.get(&config.input_field).expect("Input field not found in JSON object");
+            inputs.push(Arc::new(serde_json::from_value(input.clone()).expect("Input field is not compatible with the specified type")));
+        }
+        let size = inputs.len();
+        UnsupervisedDataset {
+            inputs,
+            size,
+            batch_size: 1,
+        }
+    }
+}
--- src/dataset/mod.rs
+++ src/dataset/mod.rs
@@ -1,5 +1,7 @@
 pub use dataset::*;
 pub use tensor_dataset::*;
+pub use load_external::*;
 
 pub mod dataset;
 pub mod tensor_dataset;
+pub mod load_external;
