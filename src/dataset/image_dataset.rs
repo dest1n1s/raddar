b954b78b3b89aad87c1ba99926e90a7adb695637
Dest1n1s <dest1nis2022@gmail.com>
2022-10-23 21:37:06 +0800

    create: ImageDataset

---
+++ src/dataset/image_dataset.rs
@@ -0,0 +1,93 @@
+use std::{ops::Deref, sync::Arc};
+
+use image::{DynamicImage, GenericImageView, ImageBuffer, Pixel};
+use tch::{kind::Element, Tensor};
+use walkdir::WalkDir;
+
+use super::{
+    data_mapping, Dataset, DatasetDataMapping, LoadFromImageFolder, UnsupervisedDataset,
+    UnsupervisedTensorDataset,
+};
+
+pub type DynImageDataset = UnsupervisedDataset<DynamicImage>;
+pub type ImageDataset<P: Pixel, Container> = UnsupervisedDataset<ImageBuffer<P, Container>>;
+
+impl LoadFromImageFolder for DynImageDataset {
+    type ConfigType = ();
+
+    fn from_image_folder(path: &str, _config: Self::ConfigType) -> Self {
+        let mut inputs = Vec::new();
+        WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
+            .into_iter()
+            .for_each(|entry| {
+                let path = entry.path();
+                let image = image::open(path).expect("Failed to open image");
+                inputs.push(Arc::new(image));
+            });
+        Self::from_vectors(inputs)
+    }
+}
+
+pub struct ImageMappings;
+
+impl ImageMappings {
+    pub fn crop_center(
+        width: u32,
+        height: u32,
+    ) -> DatasetDataMapping<
+        DynImageDataset,
+        DynImageDataset,
+        impl FnMut(<DynImageDataset as Dataset>::DataType) -> <DynImageDataset as Dataset>::DataType,
+    > {
+        data_mapping(move |input: Arc<DynamicImage>| {
+            let mut input = (*input).clone();
+            let (w, h) = input.dimensions();
+            let (x, y) = ((w - width) / 2, (h - height) / 2);
+            let cropped = input.crop(x, y, width, height);
+            Arc::new(cropped)
+        })
+    }
+
+    pub fn resize(
+        width: u32,
+        height: u32,
+    ) -> DatasetDataMapping<
+        DynImageDataset,
+        DynImageDataset,
+        impl FnMut(<DynImageDataset as Dataset>::DataType) -> <DynImageDataset as Dataset>::DataType,
+    > {
+        data_mapping(move |input: Arc<DynamicImage>| {
+            let input = (*input).clone();
+            let resized = input.resize(width, height, image::imageops::FilterType::Lanczos3);
+            Arc::new(resized)
+        })
+    }
+
+    pub fn to_tensor<
+        P: Pixel,
+        Container: Deref<Target = [P::Subpixel]> + Clone,
+        F: Fn(DynamicImage) -> ImageBuffer<P, Container>,
+    >(
+        f: F,
+    ) -> DatasetDataMapping<
+        DynImageDataset,
+        UnsupervisedTensorDataset,
+        impl FnMut(
+            <DynImageDataset as Dataset>::DataType,
+        ) -> <UnsupervisedTensorDataset as Dataset>::DataType,
+    >
+    where
+        <P as Pixel>::Subpixel: Element,
+    {
+        data_mapping(move |input: Arc<DynamicImage>| {
+            let input = (*input).clone();
+            let (w, h) = input.dimensions();
+            let input = f(input);
+            let tensor = Tensor::of_slice(&input.into_raw()).reshape(&[1, h as i64, w as i64, 3]);
+            Arc::new(tensor)
+        })
+    }
+}
--- src/dataset/tensor_dataset.rs
+++ src/dataset/tensor_dataset.rs
@@ -5,12 +5,17 @@ use tch::Tensor;
 
 use crate::core::TensorIntoIter;
 
-use super::Dataset;
+use super::{Dataset, SimpleDataset, UnsupervisedDataset, data_mapping};
 
 #[derive(Debug, Clone, DatasetIntoIter, DatasetFromIter)]
 pub struct TensorDataset {
     pub inputs: Vec<Arc<Tensor>>,
     pub labels: Vec<Arc<Tensor>>,
+}
+
+#[derive(Debug, Clone, DatasetIntoIter, DatasetFromIter)]
+pub struct UnsupervisedTensorDataset {
+    pub inputs: Vec<Arc<Tensor>>,
 }
 
 impl Dataset for TensorDataset {
@@ -46,8 +51,51 @@ impl Dataset for TensorDataset {
     }
 }
 
+impl Dataset for UnsupervisedTensorDataset {
+    type DataType = Arc<Tensor>;
+    type BatchType = Tensor;
+
+    fn data(self) -> Vec<Self::DataType> {
+        self.inputs
+    }
+
+    fn from_batches<I: IntoIterator<Item = Self::BatchType>>(batches: I) -> Self {
+        let mut inputs = Vec::new();
+        for batch_inputs in batches {
+            inputs.extend(batch_inputs.into_iter().map(Arc::new));
+        }
+        Self { inputs }
+    }
+
+    fn size(&self) -> usize {
+        self.inputs.len()
+    }
+
+    fn collate<I: IntoIterator<Item = Self::DataType>>(data: I) -> Self::BatchType {
+        Tensor::stack(&data.into_iter().collect::<Vec<_>>(), 0)
+    }
+}
+
 impl TensorDataset {
     pub fn from_tensors(inputs: Vec<Arc<Tensor>>, labels: Vec<Arc<Tensor>>) -> Self {
         Self { inputs, labels }
+    }
+}
+
+impl UnsupervisedTensorDataset {
+    pub fn from_tensors(inputs: Vec<Arc<Tensor>>) -> Self {
+        Self { inputs }
+    }
+}
+
+impl From<SimpleDataset<Tensor, Tensor>> for TensorDataset {
+    fn from(dataset: SimpleDataset<Tensor, Tensor>) -> Self {
+        dataset.map(data_mapping(|data| data))
+    }
+}
+
+impl From<UnsupervisedDataset<Tensor>> for UnsupervisedTensorDataset {
+    fn from(dataset: UnsupervisedDataset<Tensor>) -> Self {
+        dataset.map(data_mapping(|data| data))
     }
 }
