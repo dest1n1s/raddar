a6ec14522b07743fa4dc16706805a0e626e78279
Dest1n1s <dest1nis2022@gmail.com>
2022-11-08 19:42:52 +0800

    create(TensorLike): some factory method

--- src/core/tensor_like.rs
+++ src/core/tensor_like.rs
@@ -1,6 +1,10 @@
-use std::{ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign, Neg}, fmt::Debug};
+use std::{
+    borrow::Borrow,
+    fmt::Debug,
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign}, path::Path,
+};
 
-use tch::Tensor;
+use tch::{Device, Kind, Tensor};
 
 pub trait AddSelf<T> = Add<T, Output = T> + for<'a> Add<&'a T, Output = T> where T: Sized;
 pub trait AddScalar<T> = Add<f64, Output = T> + Add<i64, Output = T> where T: Sized;
@@ -88,6 +92,24 @@ pub trait DivAssignTrait = DivAssign<Self>
 
 pub trait NegTrait = Neg<Output = Self> + for<'a> Neg<Output = Self> + Sized;
 
+pub trait Element: Clone + tch::kind::Element {}
+
+impl Element for u8 {}
+impl Element for i8 {}
+impl Element for i16 {}
+impl Element for i32 {}
+impl Element for i64 {}
+impl Element for f32 {}
+impl Element for f64 {}
+impl Element for bool {}
+
+/// A trait for a tensor-like object.
+/// 
+/// A tensor-like object should have the following properties:
+/// - It should be able to perform element-wise operations, including addition, subtraction, multiplication, division, negation, and element-wise comparison, with other tensor-like objects.
+/// - It should be associated with some env variable, which for example may be used to determine the device and data type of the tensor-like object, and a size indicating the length of each dimension.
+/// - It should be able to be constructed with some factors, or other tensor-like objects.
+/// - It should also be able to perform some other operations, such as reshaping, transposing, and slicing.
 pub trait TensorLike:
     AddTrait
     + SubTrait
@@ -103,6 +125,91 @@ pub trait TensorLike:
     + Debug
     + Default
 {
+    /// The type of the env variable.
+    type Env: Default;
+
+    /// Get the env variable.
+    fn env(&self) -> Self::Env;
+
+    /// Get the size of the tensor-like object.
+    fn size(&self) -> Vec<i64>;
+
+    /// Create 
+    fn of_slice<T: Element>(data: &[T]) -> Self;
+    fn stack<T: Borrow<Self>>(tensors: &[T], dim: i64) -> Self;
+    fn cat<T: Borrow<Self>>(tensors: &[T], dim: i64) -> Self;
+    fn concat<T: Borrow<Self>>(tensors: &[T], dim: i64) -> Self;
+
+    fn zeros(size: &[i64], env: Self::Env) -> Self;
+    fn ones(size: &[i64], env: Self::Env) -> Self;
+    fn empty(size: &[i64], env: Self::Env) -> Self;
+
+    fn zeros_like(&self) -> Self {
+        Self::zeros(&self.size(), self.env())
+    }
+
+    fn ones_like(&self) -> Self {
+        Self::ones(&self.size(), self.env())
+    }
+
+    fn empty_like(&self) -> Self {
+        Self::empty(&self.size(), self.env())
+    }
+
+    fn eye(size: i64, env: Self::Env) -> Self;
+
+    fn read_npz<T: AsRef<Path>>(path: T) -> Result<Vec<(String, Tensor)>, anyhow::Error>;
+    fn read_ot<T: AsRef<Path>>(path: T) -> Result<Vec<(String, Tensor)>, anyhow::Error>;
 }
 
-impl TensorLike for Tensor {}
+impl TensorLike for Tensor {
+    type Env = (Kind, Device);
+
+    fn env(&self) -> Self::Env {
+        (self.kind(), self.device())
+    }
+
+    fn size(&self) -> Vec<i64> {
+        self.size().iter().map(|x| *x as i64).collect()
+    }
+
+    fn of_slice<T: Element>(data: &[T]) -> Self {
+        Tensor::of_slice(data)
+    }
+
+    fn stack<T: Borrow<Self>>(tensors: &[T], dim: i64) -> Self {
+        Tensor::stack(tensors, dim)
+    }
+
+    fn cat<T: Borrow<Self>>(tensors: &[T], dim: i64) -> Self {
+        Tensor::cat(tensors, dim)
+    }
+
+    fn concat<T: Borrow<Self>>(tensors: &[T], dim: i64) -> Self {
+        Tensor::concat(tensors, dim)
+    }
+
+    fn zeros(size: &[i64], env: Self::Env) -> Self {
+        Tensor::zeros(size, env)
+    }
+
+    fn ones(size: &[i64], env: Self::Env) -> Self {
+        Tensor::ones(size, env)
+    }
+
+    fn empty(size: &[i64], env: Self::Env) -> Self {
+        Tensor::empty(size, env)
+    }
+
+    fn eye(size: i64, env: Self::Env) -> Self {
+        Tensor::eye(size, env)
+    }
+
+    fn read_npz<T: AsRef<Path>>(path: T) -> Result<Vec<(String, Tensor)>, anyhow::Error> {
+        Ok(Tensor::read_npz(path)?)
+    }
+
+    fn read_ot<T: AsRef<Path>>(path: T) -> Result<Vec<(String, Tensor)>, anyhow::Error> {
+        Ok(Tensor::load_multi(path)?)
+    }
+}
