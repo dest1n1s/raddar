65e271908bc00d42cc2cfa8cfbab7605ecfd0c22
w568w <1278297578@qq.com>
2023-01-05 12:04:41 +0800

    feat(raddar_array): impl `fill()`

--- raddar_array/src/ndarr/mod.rs
+++ raddar_array/src/ndarr/mod.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused_macros)]
 use std::{
+    any::Any,
     borrow::Borrow,
     f64::consts::E,
     iter::once,
@@ -614,6 +615,10 @@ impl TensorMethods for NdArrayTensor {
         CastOp::forward(self, dtype)
     }
 
+    fn fill_<T: AnyNum>(&mut self, value: T) {
+        self.i().as_view_mut().fill_(value);
+    }
+
     fn neg(&self) -> Self {
         NegOp::forward(self)
     }
@@ -919,6 +924,10 @@ impl TensorMethods for KindedArrayD {
 
     fn cast(&self, dtype: TensorKind) -> Self {
         self.view().cast(dtype)
+    }
+
+    fn fill_<T: AnyNum>(&mut self, value: T) {
+        self.view_mut().fill_(value)
     }
 
     fn neg(&self) -> Self {
@@ -1313,6 +1322,7 @@ pub(crate) trait SuperViewMethods {
 pub(crate) trait ViewMutMethods<'this>: SuperViewMethods + 'this {
     fn upgrade(&self) -> Self::OwnedType;
 
+    fn fill_<T: AnyNum>(&mut self, value: T);
     fn slice_mut<'a>(&'a mut self, info: IndexInfo) -> Self::ViewMutType<'a>;
     fn into_slice_mut(self, info: IndexInfo) -> Self::ViewMutType<'this>;
     fn permute_mut<'a>(&'a mut self, axes: &[usize]) -> Self::ViewMutType<'a>;
@@ -1551,6 +1561,12 @@ impl SuperViewMethods for KindedArrayViewMutD<'_> {
 impl<'this> ViewMutMethods<'this> for KindedArrayViewMutD<'this> {
     fn upgrade(&self) -> Self::OwnedType {
         obtain_kind_array_view_mut!(self, array, { KindedArrayD::from(array.to_owned()) })
+    }
+
+    fn fill_<T: AnyNum>(&mut self, value: T) {
+        obtain_kind_array_view_mut!(self, array, {
+            array.fill(cast::<T, KindType>(value).unwrap())
+        })
     }
 
     fn slice_mut<'a>(&'a mut self, info: IndexInfo) -> Self::ViewMutType<'a> {
