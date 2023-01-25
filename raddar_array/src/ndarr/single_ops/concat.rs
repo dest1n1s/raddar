46687c5c0d02bf48d2b3826d9ee3bdc916c5257f
w568w <1278297578@qq.com>
2023-01-25 11:20:21 +0800

    change(raddar_array): remove duplicate methods in `concat`

--- raddar_array/src/ndarr/single_ops/concat.rs
+++ raddar_array/src/ndarr/single_ops/concat.rs
@@ -1,15 +1,20 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex, MutexGuard, Weak},
-};
+use std::sync::{Arc, Mutex, Weak};
+
+use higher_order_closure::hrtb;
+use more_asserts::assert_ge;
 
 use crate::{
     go_backward,
-    ndarr::{ops::add_grad, KindedArrayViewD, NdArrayTensor, NdArrayTensorInternal, ViewMethods},
+    ndarr::{
+        lens::{CompositeLen, LookThrough, ViewLens},
+        ops::add_grad,
+        KindedArrayD, KindedArrayViewD, NdArrayTensor, NdArrayTensorInternal, ViewMethods,
+        ViewsImmut,
+    },
     tensor::{
         index::{IndexInfo, IndexInfoItem},
         ops::Operation,
-        AutoGradTensorMethods, TensorMethods, ArrayMethods,
+        ArrayMethods, AutoGradTensorMethods, TensorMethods,
     },
 };
 pub(crate) struct CatOp {
@@ -18,49 +23,23 @@ pub(crate) struct CatOp {
     inputs: Vec<Arc<Mutex<NdArrayTensorInternal>>>,
 }
 
-/// Borrow multiple mutex guards from a slice of objects, allowing the same object to appear multiple times.
-///
-/// This function is used to borrow tensor internals from a slice of tensors.
-/// If the same tensor appears multiple times in the slice,
-/// the same lock guard is used for all of them to avoid deadlocks.
-///
-/// todo: replace the `borrow_three_tensor_internals` macro with this function and remove those macros.
-fn borrow_arc_mutexes<T, U>(inputs: &[&Arc<Mutex<U>>], f: impl Fn(&[&MutexGuard<U>]) -> T) -> T {
-    let mut guards = HashMap::with_capacity(inputs.len());
-    let mut guard_refs = Vec::with_capacity(inputs.len());
-    for (i, input) in inputs.iter().enumerate() {
-        let duplicated = inputs
-            .iter()
-            .enumerate()
-            .take(i)
-            .find(|(_, x)| Arc::ptr_eq(x, input));
-        if let Some((j, _)) = duplicated {
-            guard_refs.push(j);
-        } else {
-            guards.insert(i, input.lock().expect("The mutex is poisoned"));
-            guard_refs.push(i);
-        }
-    }
-    let guard_refs = guard_refs
-        .into_iter()
-        .map(|i| &guards[&i])
-        .collect::<Vec<_>>();
-    f(guard_refs.as_slice())
-}
-
 impl CatOp {
     pub fn forward(inputs: &[&NdArrayTensor], dim: usize) -> NdArrayTensor {
-        let inputs_i = inputs
-            .iter()
-            .map(|x| x.internal.as_ref().unwrap())
-            .collect::<Vec<_>>();
-        let mut result: NdArrayTensor = borrow_arc_mutexes(inputs_i.as_slice(), |inputs| {
-            let inputs = inputs
-                .into_iter()
-                .map(|x| x.as_view().clone())
-                .collect::<Vec<_>>();
-            KindedArrayViewD::cat(&inputs, dim).into()
-        });
+        assert_ge!(inputs.len(), 1, "CatOp requires at least one input");
+
+        let mut len = ViewLens::with(inputs[0].i_copy());
+        for input in inputs.iter().skip(1) {
+            len = len.and(input.i_copy());
+        }
+        let mut result: NdArrayTensor = len
+            .look_through(hrtb!(|inputs: ViewsImmut<'_, '_>| -> KindedArrayD {
+                KindedArrayViewD::cat(
+                    &inputs.iter().map(|handle| &**handle).collect::<Vec<_>>(),
+                    dim,
+                )
+            }))
+            .into();
+
         if inputs.iter().any(|x| x.requires_grad()) {
             result.i().op = Some(Arc::new(CatOp {
                 dim,
